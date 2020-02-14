56206c907116069535cb7bf59bd0568b8ef97ff4
eriksorthvid <erik@sorthvid.net>
2020-02-14 09:43:57 +0100

    dynamic module export tryouts

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
-import './styles/index.scss'
+import "./styles/index.scss";
+
+import Spinner from "./components/spinner/spinner";
+import Heading1 from "./components/heading-1/heading-1";
+import Heading2 from "./components/heading-2/heading-2";
+import Hero from "./components/hero/hero";
+
+export { Spinner, Heading1, Heading2, Hero };
 
 
-// idea (psuedo code):
+// // idea (psuedo code):
 
-/**
- * Import all files matching '.js' pattern.
- * 
- * To dynamically create import/export,
- * without having to manually specify them.
- * 
- * note:
- * exports has to be avaiable as is,
- * without any compiling. can this be done?
- */
+// /**
+//  * Import all files matching '.js' pattern.
+//  * 
+//  * To dynamically create import/export,
+//  * without having to manually specify them.
+//  * 
+//  * note:
+//  * exports has to be avaiable as is,
+//  * without any compiling. can this be done?
+//  */
 
-// https://webpack.js.org/guides/dependency-management/:
+// // https://webpack.js.org/guides/dependency-management/:
 
+// const cache = {}
+// const exportObj = {}
 
-const cache = {}
+// function importAll(r) {
+//     r.keys().forEach(key => cache[key] = r(key))
+// }
 
-function importAll(r) {
-    r.keys().forEach(key => cache[key] = r(key))
-}
+// function exportAll(r) {
+//     r.keys().forEach((key, i) => exportObj['key' + i] = r(key))
+// }
 
-// get all js-files from src folder: (exclude .html and .stories)
-importAll(require.context("./components/", true, /^(?!.*(?:html.js|stories.js$)).*\.js$/))
+// // get all js-files from src folder: (exclude .html.js and .stories.js)
+// const jsFiles = require.context("./components/", true, /^(?!.*(?:html.js|stories.js$)).*\.js$/)
+// importAll(jsFiles)
+// // const modules = jsFiles.keys().map(jsFiles)
+// exportAll(jsFiles)
 
-// export them:
-export { cache }
+// console.log('// dsb_designsystem: export default ', exportObj)
+
+// // export them:
+// export default exportObj
